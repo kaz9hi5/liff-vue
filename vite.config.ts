264628dc62c4786f1c1d558f352ac0fda52e8b80
kaz9hi5 <yilongbihu@gmail.com>
2023-08-26 19:22:32 +0900

    Liff 2.22.2 practice

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  preview: {
+    port: 3000
+  },
+  server: {
+    port: 3000
+  }
+});
